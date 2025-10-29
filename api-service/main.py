ae8967046c05ed8489ed395b6786674d3840d998
MatthildurMargret <mma29@stanford.edu>
2025-10-29 15:04:33 -0700

    Return dicts instead

--- api-service/main.py
+++ api-service/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Header
 import psycopg2, os
 import math
 from fastapi.middleware.cors import CORSMiddleware
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
@@ -13,7 +14,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-conn = psycopg2.connect(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(os.environ["DATABASE_URL"], cursor_factory=RealDictCursor)
 
 
 def sanitize_value(v):
@@ -23,32 +24,33 @@ def sanitize_value(v):
 
 
 @app.get("/recommended-founders")
-def get_data(x_api_key: str = Header(None)):
+def get_recommended_founders(x_api_key: str = Header(None)):
     if x_api_key != os.getenv("API_KEY"):
         raise HTTPException(status_code=403)
     with conn.cursor() as cur:
         cur.execute("""
-            SELECT DISTINCT ON (name) *
+            SELECT DISTINCT ON (name)
+                company_name, tree_path, company_tags
             FROM founders
             WHERE founder = true AND history = 'recommended'
             ORDER BY name, id DESC;
         """)
         rows = cur.fetchall()
-        clean_rows = [[sanitize_value(v) for v in row] for row in rows]
-    return {"data": clean_rows}
+    return {"data": rows}
 
 
 @app.get("/unseen-founders")
-def get_data(x_api_key: str = Header(None)):
+def get_unseen_founders(x_api_key: str = Header(None)):
     if x_api_key != os.getenv("API_KEY"):
         raise HTTPException(status_code=403)
     with conn.cursor() as cur:
         cur.execute("""
-            SELECT DISTINCT ON (name) *
+            SELECT DISTINCT ON (name)
+                company_name, tree_path, company_tags
             FROM founders
-            WHERE founder = true AND history = '' AND (tree_result = 'Strong recommend' OR tree_result = 'Recommend')
+            WHERE founder = true AND history = '' 
+              AND (tree_result = 'Strong recommend' OR tree_result = 'Recommend')
             ORDER BY name, id DESC;
         """)
         rows = cur.fetchall()
-        clean_rows = [[sanitize_value(v) for v in row] for row in rows]
-    return {"data": clean_rows}
+    return {"data": rows}
