7e68ab8b68000d62011fc0d14835d25d5004f6ca
MatthildurMargret <mma29@stanford.edu>
2025-10-29 14:12:54 -0700

    First two endpoints set

--- api-service/main.py
+++ api-service/main.py
@@ -4,11 +4,21 @@ import psycopg2, os
 app = FastAPI()
 conn = psycopg2.connect(os.environ["DATABASE_URL"], sslmode="require")
 
-@app.get("/data")
+@app.get("/recommended-founders")
 def get_data(x_api_key: str = Header(None)):
     if x_api_key != os.getenv("API_KEY"):
         raise HTTPException(status_code=403)
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM your_table ORDER BY id DESC LIMIT 100;")
+        cur.execute("SELECT DISTINCT ON (name) * FROM founders WHERE founder = true AND history = 'recommended' ORDER BY id DESC;")
+        rows = cur.fetchall()
+    return {"data": rows}
+
+
+@app.get("/unseen-founders")
+def get_data(x_api_key: str = Header(None)):
+    if x_api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=403)
+    with conn.cursor() as cur:
+        cur.execute("SELECT DISTINCT ON (name) * FROM founders WHERE founder = true AND history = '' AND (tree_result = 'Strong recommend' OR tree_result = 'Recommend') ORDER BY id DESC;")
         rows = cur.fetchall()
     return {"data": rows}
