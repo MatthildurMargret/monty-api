dd9f842881a285b630b31cbd9f10ffbb7b3f051c
MatthildurMargret <mma29@stanford.edu>
2025-10-29 14:25:01 -0700

    Fixed sql query

--- api-service/main.py
+++ api-service/main.py
@@ -9,7 +9,7 @@ def get_data(x_api_key: str = Header(None)):
     if x_api_key != os.getenv("API_KEY"):
         raise HTTPException(status_code=403)
     with conn.cursor() as cur:
-        cur.execute("SELECT DISTINCT ON (name) * FROM founders WHERE founder = true AND history = 'recommended' ORDER BY id DESC;")
+        cur.execute("SELECT DISTINCT ON (name) * FROM founders WHERE founder = true AND history = 'recommended' ORDER BY name, id DESC;")
         rows = cur.fetchall()
     return {"data": rows}
 
@@ -19,6 +19,6 @@ def get_data(x_api_key: str = Header(None)):
     if x_api_key != os.getenv("API_KEY"):
         raise HTTPException(status_code=403)
     with conn.cursor() as cur:
-        cur.execute("SELECT DISTINCT ON (name) * FROM founders WHERE founder = true AND history = '' AND (tree_result = 'Strong recommend' OR tree_result = 'Recommend') ORDER BY id DESC;")
+        cur.execute("SELECT DISTINCT ON (name) * FROM founders WHERE founder = true AND history = '' AND (tree_result = 'Strong recommend' OR tree_result = 'Recommend') ORDER BY name, id DESC;")
         rows = cur.fetchall()
     return {"data": rows}
